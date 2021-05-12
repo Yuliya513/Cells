d5c432d1fb77acffd6111ea4e5d24c25172a9bc8
Yuliya513 <yuliya.senik@gmail.com>
2021-05-12 17:19:55 +0300

    toString methods

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public  class Main {
+
+    public static void main(String[] args) {
+	String FirstName="Yu";
+	String LastName="Se";
+	int age=20;
+
+
+		System.out.println("I");
+		System.out.println("age");
+		System.out.println("Yu");
+		System.out.println("Se");
+
+
+	System.out.println(FirstName +LastName+ age);
+
+
+    }
+}
